817ca42b825e36c73f00ece9b746b420c9e8c856
alamgir_hosen <thisisalamgir@gmail.com>
2024-08-08 21:54:32 +0600

    added video with transition

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+let time = 5000;
 document.addEventListener("DOMContentLoaded", function () {
   setTimeout(function () {
     document.getElementById("to_welcome").classList.add("hidden");
-  }, 1000); // 10000 milliseconds = 10 seconds
+  }, time); // 10000 milliseconds = 10 seconds
+});
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(function () {
+    document.getElementById("welcome_video").classList.add("visible");
+  }, time); // 10000 milliseconds = 10 seconds
 });
