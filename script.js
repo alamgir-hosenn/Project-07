fdb0f0fd73f8063e2a70b32a6c869b9d87195c19
alamgir_hosen <thisisalamgir@gmail.com>
2024-08-08 20:43:52 +0600

    ui: created all layout n add js for welcome page

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(function () {
+    document.getElementById("to_welcome").classList.add("hidden");
+  }, 1000); // 10000 milliseconds = 10 seconds
+});
