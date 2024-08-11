cc0daa35f557ad59475499786a4a855e1d01197a
alamgir_hosen <thisisalamgir@gmail.com>
2024-08-11 15:19:43 +0600

    added video section n desinged

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-let time = 5000;
-document.addEventListener("DOMContentLoaded", function () {
-  setTimeout(function () {
-    document.getElementById("to_welcome").classList.add("hidden");
-  }, time); // 10000 milliseconds = 10 seconds
-});
-document.addEventListener("DOMContentLoaded", function () {
-  setTimeout(function () {
-    document.getElementById("welcome_video").classList.add("visible");
-  }, time); // 10000 milliseconds = 10 seconds
+let show_video = document.getElementById("show_video");
+let video_container = document.getElementById("video_container");
+
+show_video.addEventListener("click", () => {
+  show_video.classList.toggle("active_button");
+  video_container.classList.toggle("active_video");
 });
